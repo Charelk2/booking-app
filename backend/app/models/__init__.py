19812e56c5d9d5274922420935f365dbdf85f527
Charelk2 <Charelk@gmail.com>
2025-06-04 11:02:18 +0200

    Export user and booking enums

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,9 +1,9 @@
-from .user import User
+from .user import User, UserType
 from .artist_profile_v2 import ArtistProfileV2 as ArtistProfile
 from .service import Service
-from .booking import Booking
+from .booking import Booking, BookingStatus
 from .review import Review
-from .request_quote import BookingRequest, Quote
+from .request_quote import BookingRequest, Quote, BookingRequestStatus, QuoteStatus
 from .sound_provider import SoundProvider
 from .artist_sound_preference import ArtistSoundPreference
 
@@ -17,4 +17,8 @@ __all__ = [
     "Quote",
     "SoundProvider",
     "ArtistSoundPreference",
+    "UserType",
+    "BookingStatus",
+    "BookingRequestStatus",
+    "QuoteStatus",
 ]
