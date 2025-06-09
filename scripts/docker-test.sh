0f1c7f84876ab88c4c51f44b8ed6742094ae25ae
Charelk2 <Charelk@gmail.com>
2025-06-09 20:37:15 +0200

    Document Docker helper usage

--- scripts/docker-test.sh
+++ scripts/docker-test.sh
@@ -2,9 +2,10 @@
 set -euxo pipefail
 IMAGE=${BOOKING_APP_IMAGE:-ghcr.io/example-org/booking-app-ci:latest}
 SCRIPT=${TEST_SCRIPT:-./scripts/test-all.sh}
+NETWORK=${DOCKER_TEST_NETWORK:-none}
 
 echo "Pulling $IMAGE"
 docker pull "$IMAGE"
 
 echo "Running tests in $IMAGE"
-docker run --rm -v "$(pwd)":/app "$IMAGE" bash -lc "$SCRIPT"
+docker run --rm --network "$NETWORK" -v "$(pwd)":/app "$IMAGE" bash -lc "$SCRIPT"
