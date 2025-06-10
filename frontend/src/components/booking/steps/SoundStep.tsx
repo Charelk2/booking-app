05caa60dde7a5721284effc1f17f35a3aeeb55ad
Charelk2 <Charelk@gmail.com>
2025-06-10 23:35:16 +0200

    Refactor booking wizard UI

--- frontend/src/components/booking/steps/SoundStep.tsx
+++ frontend/src/components/booking/steps/SoundStep.tsx
@@ -8,35 +8,33 @@ interface Props {
 export default function SoundStep({ control }: Props) {
   return (
     <div className="space-y-4">
-      <label className="block mb-2 font-medium">Is sound needed?</label>
       <Controller
         name="sound"
         control={control}
         render={({ field }) => (
-          <div className="space-y-2">
-            <label>
+          <fieldset className="space-y-2">
+            <legend className="font-medium">Is sound needed?</legend>
+            <label className="flex items-center space-x-2">
               <input
                 type="radio"
                 name={field.name}
                 value="yes"
                 checked={field.value === 'yes'}
                 onChange={(e) => field.onChange(e.target.value)}
-                className="mr-1"
               />
-              Yes
+              <span>Yes</span>
             </label>
-            <label>
+            <label className="flex items-center space-x-2">
               <input
                 type="radio"
                 name={field.name}
                 value="no"
                 checked={field.value === 'no'}
                 onChange={(e) => field.onChange(e.target.value)}
-                className="mr-1"
               />
-              No
+              <span>No</span>
             </label>
-          </div>
+          </fieldset>
         )}
       />
     </div>
