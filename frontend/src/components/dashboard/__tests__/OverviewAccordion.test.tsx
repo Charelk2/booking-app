ec8be0717153ff4218239f809856e86520d3516b
Charelk2 <Charelk@gmail.com>
2025-06-10 16:13:58 +0200

    test: import act from react

--- frontend/src/components/dashboard/__tests__/OverviewAccordion.test.tsx
+++ frontend/src/components/dashboard/__tests__/OverviewAccordion.test.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-import { act } from 'react-dom/test-utils';
+import { act } from 'react';
 import OverviewAccordion from '../OverviewAccordion';
 
 describe('OverviewAccordion', () => {
