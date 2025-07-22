8f8c75c4e257ad62cd3c202da45bdc28c199d0a8
Charelk2 <Charelk@gmail.com>
2025-07-22 12:53:36 +0200

    test(frontend): add snapshot for ArtistsSection

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,14 @@
 import MainLayout from '@/components/layout/MainLayout'
 import Hero from '@/components/layout/Hero'
+import ArtistsSection from '@/components/home/ArtistsSection'
 
 export default function HomePage() {
   return (
     <MainLayout>
       <Hero />
+      <ArtistsSection title="Popular Musicians" query={{ sort: 'popular' }} />
+      <ArtistsSection title="Top Rated" query={{ sort: 'rating_desc' }} />
+      <ArtistsSection title="New on Booka" query={{ sort: 'created_desc' }} />
     </MainLayout>
   )
 }
