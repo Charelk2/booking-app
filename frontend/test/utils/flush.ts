6b24023fbaaa46a7d82d41297543fe234d420cfc
Charelk2 <Charelk@gmail.com>
2025-07-22 23:16:40 +0200

    chore: adjust test utilities and scripts

--- frontend/test/utils/flush.ts
+++ frontend/test/utils/flush.ts
@@ -1,13 +1,7 @@
 import { act } from 'react';
-
 export async function flushPromises() {
-  await act(async () => {
-    await Promise.resolve();
-  });
+  await act(async () => { await Promise.resolve(); });
   if (jest.isMockFunction(setTimeout)) {
-    act(() => {
-      jest.runOnlyPendingTimers();
-    });
+    act(() => { jest.runOnlyPendingTimers(); });
   }
 }
-
