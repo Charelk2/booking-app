c6c78cc67eaba8c86c4f0693a527967d60edd8ce
Charelk2 <Charelk@gmail.com>
2025-07-26 17:30:21 +0200

    feat: modern add service wizard

--- frontend/src/styles/buttonVariants.ts
+++ frontend/src/styles/buttonVariants.ts
@@ -2,6 +2,8 @@ export const buttonVariants = {
   primary: 'bg-brand hover:bg-brand-dark text-white focus:ring-brand-dark',
   secondary:
     'bg-white border border-gray-300 text-gray-800 hover:bg-gray-50 focus:ring-gray-300',
+  outline:
+    'bg-transparent border border-gray-300 text-gray-800 hover:bg-gray-50 focus:ring-gray-300',
   danger: 'bg-red-600 text-white hover:bg-red-700 focus:ring-red-500',
   link:
     'bg-transparent underline text-brand-dark hover:text-brand-dark focus:ring-brand px-0 py-0',
