a648ed9ec4af9b262d1ba10df2b6f7cad8c9edf5
Charelk2 <Charelk@gmail.com>
2025-07-26 11:31:58 +0200

    Replace gmpx location input with shared component

--- frontend/src/components/booking/steps/__tests__/LocationStep.test.tsx
+++ frontend/src/components/booking/steps/__tests__/LocationStep.test.tsx
@@ -4,6 +4,31 @@ import { act } from 'react';
 import { useForm, Control, FieldValues } from 'react-hook-form';
 import LocationStep from '../LocationStep';
 
+jest.mock('react-google-autocomplete/lib/usePlacesAutocompleteService', () => {
+  const React = require('react');
+  return () => {
+    const [preds, setPreds] = React.useState<any[]>([]);
+    return {
+      placesService: {
+        getDetails: (_opts: any, cb: (p: any) => void) =>
+          cb({
+            geometry: { location: { lat: () => 1, lng: () => 2 } },
+            formatted_address: 'Test',
+          }),
+      },
+      placePredictions: preds,
+      getPlacePredictions: () =>
+        setPreds([
+          {
+            place_id: '1',
+            description: 'Test',
+            structured_formatting: { main_text: 'Test', secondary_text: 'SA' },
+          },
+        ]),
+    };
+  };
+});
+
 function Wrapper() {
   const { control } = useForm();
   return (
@@ -41,14 +66,14 @@ describe('LocationStep selection', () => {
     await act(async () => {
       root.render(React.createElement(Wrapper));
     });
-    const mock = (global as { mockAutocomplete: jest.Mock }).mockAutocomplete;
-    const instance = mock.mock.instances[0];
-    instance.getPlace.mockReturnValue({
-      geometry: { location: { lat: () => 1, lng: () => 2 } },
-      formatted_address: 'Test',
-    });
+    const input = container.querySelector('input') as HTMLInputElement;
     await act(async () => {
-      instance._cb();
+      input.value = 'Test';
+      input.dispatchEvent(new Event('input', { bubbles: true }));
+    });
+    const option = container.querySelector('[data-testid="location-option"]') as HTMLDivElement;
+    await act(async () => {
+      option.dispatchEvent(new MouseEvent('click', { bubbles: true }));
     });
     expect(container.querySelector('[data-testid="map"]')).not.toBeNull();
   });
