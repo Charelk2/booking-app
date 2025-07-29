f3f5e51d6613cccccd15bfab840e0b1c75abcc7a
Charelk2 <Charelk@gmail.com>
2025-07-29 10:54:04 +0200

    Revert "Update login prompts"

--- frontend/src/app/quotes/[quoteId]/page.tsx
+++ frontend/src/app/quotes/[quoteId]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import Link from 'next/link';
 import { useParams } from 'next/navigation';
 import MainLayout from '@/components/layout/MainLayout';
 import QuoteCard from '@/components/booking/QuoteCard';
@@ -35,16 +34,7 @@ export default function QuoteDetailPage() {
   if (!user) {
     return (
       <MainLayout>
-        <div className="p-8">
-          Please{' '}
-          <Link
-            href="/login"
-            className="text-brand-dark no-underline hover:no-underline"
-          >
-            log in
-          </Link>{' '}
-          to view this quote.
-        </div>
+        <div className="p-8">Please log in to view this quote.</div>
       </MainLayout>
     );
   }
