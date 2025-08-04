485040f23f63afd6920bef8a38ffa2bdc0e6f086
Charelk2 <Charelk@gmail.com>
2025-08-04 23:36:51 +0200

    Revert "Show artist business name on booking requests"

--- backend/app/crud/crud_booking_request.py
+++ backend/app/crud/crud_booking_request.py
@@ -26,12 +26,7 @@ def create_booking_request(
 def get_booking_request(db: Session, request_id: int) -> Optional[models.BookingRequest]:
     return (
         db.query(models.BookingRequest)
-        .options(
-            joinedload(models.BookingRequest.quotes),
-            joinedload(models.BookingRequest.artist).joinedload(
-                models.User.artist_profile
-            ),
-        )
+        .options(joinedload(models.BookingRequest.quotes))
         .filter(models.BookingRequest.id == request_id)
         .first()
     )
@@ -125,12 +120,8 @@ def get_booking_requests_with_last_message(
         db.query(models.BookingRequest)
         .options(
             joinedload(models.BookingRequest.client),
-            joinedload(models.BookingRequest.artist).joinedload(
-                models.User.artist_profile
-            ),
-            joinedload(models.BookingRequest.service).joinedload(
-                models.Service.artist
-            ),
+            joinedload(models.BookingRequest.artist),
+            joinedload(models.BookingRequest.service),
             joinedload(models.BookingRequest.quotes),
         )
         .outerjoin(latest_msg, models.BookingRequest.id == latest_msg.c.br_id)
