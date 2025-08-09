f76bb8d323745aeac0ca0a0b9cbf8e527eee2343
Charelk2 <Charelk@gmail.com>
2025-08-09 10:51:50 +0200

    Add cascade deletes for service relations

--- backend/app/models/review.py
+++ backend/app/models/review.py
@@ -8,7 +8,7 @@ class Review(BaseModel):
 
     id = Column(Integer, primary_key=True, index=True)
     booking_id  = Column(Integer, ForeignKey("bookings.id"), nullable=False)
-    service_id  = Column(Integer, ForeignKey("services.id"), nullable=False)
+    service_id  = Column(Integer, ForeignKey("services.id", ondelete="CASCADE"), nullable=False)
     artist_id   = Column(Integer, ForeignKey("artist_profiles.user_id"), nullable=False)
 
     rating      = Column(Integer, nullable=False)
