123cd2923509589437610a69f0fe528e84d3c70b
Charelk2 <charelk@gmail.com>
2025-11-21 15:46:45 +0200

    Commit message describing the changes2

--- frontend/src/app/service-providers/[id]/ProfileClient.tsx
+++ frontend/src/app/service-providers/[id]/ProfileClient.tsx
@@ -93,7 +93,7 @@ function ReviewStars({ rating }: { rating: number }) {
   return (
     <div className="flex items-center">
       {[...Array(5)].map((_, i) => (
-        <StarSolidIcon key={i} className={`h-2 w-2 ${i < full ? 'text-black' : 'text-gray-300'}`} />
+        <StarSolidIcon key={i} className={`h-2.5 w-2.5 ${i < full ? 'text-black' : 'text-gray-300'}`} />
       ))}
     </div>
   );
