05ef4410ae276cd41619d5433b247e7002ec41ad
Charelk2 <charelk@gmail.com>
2025-11-22 11:10:00 +0200

    Commit message describing the changes1.1222

--- frontend/src/components/home/CategoriesCarousel.tsx
+++ frontend/src/components/home/CategoriesCarousel.tsx
@@ -178,7 +178,7 @@ export default function CategoriesCarousel() {
   };
 
   return (
-    <section className="full-width mx-auto mt-4 px-4 sm:px-6 lg:px-8" aria-labelledby="categories-heading">
+    <section className="full-width mx-auto mt-0 sm:mt-4 px-4 sm:px-6 lg:px-8" aria-labelledby="categories-heading">
       <div className="flex items-center justify-between">
         <h2 id="categories-heading" className="text-xl font-semibold">Services Near You</h2>
         <div className="hidden sm:flex gap-2">
